13d10ee222eae43ea3082a87bab36cac83f0934f
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-07-17 17:14:24 +0300

    Allow admin login to server

--- index.js
+++ index.js
@@ -43,8 +43,16 @@ io.use(function (socket, next) {
   const username = socket.handshake.auth.username;
   // ensure required params
   if (!userId) {
-    console.log("Cannot join: Missing userId")
+    console.log("Cannot join: Missing userId");
     return next(new Error("Missing userId"));
+  }
+
+  // check if it's admin login
+  if (userId == "admin") {
+    socket.userId = "admin";
+    socket.username = "Admin";
+    socket.email = "admin@gmail.com";
+    return next();
   }
 
   sessionStore.findSession(userId, (user) => {
@@ -93,67 +101,77 @@ io.use(function (socket, next) {
 });
 
 // fired on connection
-io.on("connection", socket => {
+io.on("connection", (socket) => {
+  console.log(`New User join: ${socket.username}`);
+  // user has connected. Notify other users
+  socket.broadcast.emit("user-connected", {
+    userId: socket.userId,
+    username: socket.username,
+    email: socket.email,
+    connected: true,
+  });
 
-    console.log(`New User join: ${socket.username}`);
-    // user has connected. Notify other users
-    socket.broadcast.emit("user-connected", {
-        userId: socket.userId, 
-        username: socket.username,
-        email: socket.email,
-        connected: true
+  // send data about all users and their online status
+  let allUsers = [];
+  sessionStore.findAllUsers((users) => {
+    const values = users.values();
+    let user;
+    while ((user = values.next().value) != undefined) {
+      allUsers.push({
+        userId: user.u_id,
+        username: user.username,
+        email: user.email,
+        connected: Boolean(user.connected),
+        lastSeen: user.last_seen,
+      });
+    }
+
+    // if admin is logged in, send him as a user too
+    const connectedSockets = io.sockets.sockets;
+    let isAdminConnected = false;
+    connectedSockets.forEach((socket) => {
+      if (socket.userId == "admin") {
+        isAdminConnected = true;
+        return;
+      }
     });
 
-    // send data about all users and their online status
-    let allUsers = [];
-    sessionStore.findAllUsers(users => {
-        const values = users.values();
-        let user;
-        while((user = values.next().value) != undefined){
-
-            allUsers.push({
-                userId: user.u_id,
-                username: user.username,
-                email: user.email,
-                connected: Boolean(user.connected),
-                lastSeen: user.last_seen
-            })
-        };
-
-         socket.emit("users", allUsers);
+    allUsers.push({
+      userId: "admin",
+      username: "Admin",
+      email: "admi@mail.com",
+      connected: isAdminConnected,
+      last_seen: "",
     });
 
-   
+    socket.emit("users", allUsers);
+  });
 
-    // broadcast selection results
-    socket.on("selection-results", data => {
-      console.log("selection results", data);
-        socket.broadcast.emit("selection-results", data);
+  // broadcast selection results
+  socket.on("selection-results", (data) => {
+    console.log("selection results", data);
+    socket.broadcast.emit("selection-results", data);
+  });
+
+  // notify of user left
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", {
+      userId: socket.userId,
+      username: socket.username,
     });
 
-    // notify of user left
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("user-disconnected", {
-          userId: socket.userId,
-          username: socket.username
-        });
+    // update in database
+    const now = new Date();
 
-        // update in database
-        const now = new Date();
+    // don't update database for admin
+    if (socket.userId == "admin") return;
 
-        sessionStore.updateSession(socket.userId, {
-            connected: false, 
-            last_seen: now
-        });
-
-
-    })
-
-
+    sessionStore.updateSession(socket.userId, {
+      connected: false,
+      last_seen: now,
+    });
+  });
 });
-
-
-
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
